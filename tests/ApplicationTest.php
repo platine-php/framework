de6943016a2e6d83dec1f38596300a2789d67a36
Tony NGUEREZA <nguerezatony@gmail.com>
2021-02-24 09:59:24 +0100

    initial commit

---
+++ tests/ApplicationTest.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Platine\Test;
+
+use Platine\Application;
+use Platine\PlatineTestCase;
+
+/**
+ * Application class tests
+ *
+ * @group core
+ * @group framework
+ */
+class ApplicationTest extends PlatineTestCase
+{
+
+    public function testConstructor(): void
+    {
+        $this->assertTrue(true);
+    }
+}
