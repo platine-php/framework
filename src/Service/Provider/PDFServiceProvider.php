8a609963948199ecf7568529bb25f8017346516f
Tony NGUEREZA <nguerezatony@gmail.com>
2021-09-04 09:23:45 +0100

    Add PDF generation support

---
+++ src/Service/Provider/PDFServiceProvider.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * Platine Framework
+ *
+ * Platine Framework is a lightweight, high-performance, simple and elegant
+ * PHP Web framework
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2020 Platine Framework
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/**
+ *  @file PDFServiceProvider.php
+ *
+ *  The Framework PDF generation service provider class
+ *
+ *  @package    Platine\Framework\Service\Provider
+ *  @author Platine Developers team
+ *  @copyright  Copyright (c) 2020
+ *  @license    http://opensource.org/licenses/MIT  MIT License
+ *  @link   http://www.iacademy.cf
+ *  @version 1.0.0
+ *  @filesource
+ */
+
+declare(strict_types=1);
+
+namespace Platine\Framework\Service\Provider;
+
+use Dompdf\Dompdf;
+use Platine\Container\ContainerInterface;
+use Platine\Framework\Service\ServiceProvider;
+use Platine\PDF\Generator\DOMPDFGenerator;
+use Platine\PDF\PDF;
+use Platine\PDF\PDFGeneratorInterface;
+
+/**
+ * @class PDFServiceProvider
+ * @package Platine\Framework\Service\Provider
+ */
+class PDFServiceProvider extends ServiceProvider
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function register(): void
+    {
+        $this->app->bind(Dompdf::class, function (ContainerInterface $app) {
+            $defaultOptions = array(
+                'isRemoteEnabled' => true
+            );
+            return new Dompdf($defaultOptions);
+        });
+        $this->app->bind(PDFGeneratorInterface::class, DOMPDFGenerator::class);
+        $this->app->bind(PDF::class);
+    }
+}
